d2dfd61f37621a521e9fbf90d251e20c72660875
jomelexis <jomelexis21@gmail.com>
2024-09-13 22:39:16 +0800

    Update index.php

--- index.php
+++ index.php
@@ -157,7 +157,7 @@
     <!-- ***** Call to Action End ***** -->
     
          <!-- ***** Testimonials Starts - VIERNES ***** -->
-         <section class="section" id="trainers_rgv">
+    <section class="section" id="trainers_rgv">
         <div class="container">
             <div class="row">
                 <div class="col-lg-6 offset-lg-3">
@@ -168,6 +168,7 @@
                     </div>
                 </div>
             </div>
+
             <div class="row">
                 <div class="col-lg-4">
                     <div class="trainer-item">
@@ -187,6 +188,7 @@
                         </div>
                     </div>
                 </div>
+
                 <div class="col-lg-4">
                     <div class="trainer-item">
                         <div class="image-thumb">
@@ -205,11 +207,13 @@
                         </div>
                     </div>
                 </div>
+
                 <div class="col-lg-4">
                     <div class="trainer-item">
                         <div class="image-thumb">
                             <img src="assets/images/third-trainer.jpg" alt="">
                         </div>
+                    
                         <div class="down-content">
                             <span>Power Trainer</span>
                             <h4>Paul D. Newman</h4>
@@ -237,6 +241,7 @@
                       <iframe src="https://maps.google.com/maps?q=Av.+L%C3%BAcio+Costa,+Rio+de+Janeiro+-+RJ,+Brazil&t=&z=13&ie=UTF8&iwloc=&output=embed" width="100%" height="600px" frameborder="0" style="border:0" allowfullscreen></iframe>
                     </div>
                 </div>
+
                 <div class="col-lg-6 col-md-6 col-xs-12">
                     <div class="contact-form">
                         <form id="contact" action="" method="post">
@@ -246,21 +251,25 @@
                                 <input name="name" type="text" id="name" placeholder="Your Name*" required="">
                               </fieldset>
                             </div>
+
                             <div class="col-md-6 col-sm-12">
                               <fieldset>
                                 <input name="email" type="text" id="email" pattern="[^ @]*@[^ @]*" placeholder="Your Email*" required="">
                               </fieldset>
                             </div>
+
                             <div class="col-md-12 col-sm-12">
                               <fieldset>
                                 <input name="subject" type="text" id="subject" placeholder="Subject">
                               </fieldset>
                             </div>
+
                             <div class="col-lg-12">
                               <fieldset>
                                 <textarea name="message" rows="6" id="message" placeholder="Message" required=""></textarea>
                               </fieldset>
                             </div>
+                            
                             <div class="col-lg-12">
                               <fieldset>
                                 <button type="submit" id="form-submit" class="main-button">Send Message</button>
